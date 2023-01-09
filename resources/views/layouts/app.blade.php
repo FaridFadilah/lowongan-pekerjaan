0df05d3e4815bf4bd57f38f106d715a7906d36b9
farid fadilah permana <faridfadillah29@gmail.com>
2023-01-09 20:24:23 +0700

    bisa pliss

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,9 +9,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
     <body>
-        @if(request()->path() != request()->is('auth/*'))
             <livewire:components.navbar/>
-        @endif
         {{ $slot }}
         @livewireScripts
     </body>
